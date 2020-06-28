a15232a061649faf6a0c623985e77ff6c5499456
neo <sjkyspa@gmail.com>
2020-06-28 23:51:20 +0800

    feat(06): add compiler project

---
+++ projects/06/compiler/test/tokenizer.cpp
@@ -0,0 +1,11 @@
+#include "tokenizer.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+TEST(tokenizer, should_able_to_parse_def_token) {
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
