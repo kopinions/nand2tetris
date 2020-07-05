a86fb3aea9d76c6701a03bf68a988ed5ecd97fc7
neo <sjkyspa@gmail.com>
2020-07-05 17:16:45 +0800

    feat: add the default symbol in context

--- projects/06/compiler/include/context.hpp
+++ projects/06/compiler/include/context.hpp
@@ -8,7 +8,7 @@
 
 class context {
 public:
-  context() : _symbols(), _variable_it(16), _freed_variable_location() {
+  context() : _symbols(), _variable_it(16), _freed_variable_location(), _preserved() {
     for (auto i = 0; i < 16; i++) {
       _symbols["R" + std::to_string(i)] = i;
     }
@@ -19,6 +19,17 @@ public:
     _symbols["ARG"] = 2;
     _symbols["THIS"] = 3;
     _symbols["THAT"] = 4;
+
+    for (auto i = 0; i < 16; i++) {
+      _preserved.push_back("R" + std::to_string(i));
+    }
+    _preserved.push_back("SCREEN");
+    _preserved.push_back("KBD");
+    _preserved.push_back("SP");
+    _preserved.push_back("LCL");
+    _preserved.push_back("ARG");
+    _preserved.push_back("THIS");
+    _preserved.push_back("THAT");
   };
   virtual ~context() = default;
   std::optional<int> defined(std::string name) {
@@ -29,6 +40,10 @@ public:
     }
   };
   virtual void define(std::string name, int location = -1) {
+    if (std::find(_preserved.begin(), _preserved.end(), name) != _preserved.end()) {
+      return;
+    }
+
     if (location == -1) {
       if (_freed_variable_location.empty()) {
         _symbols[name] = ++_variable_it;
@@ -48,5 +63,7 @@ private:
   std::map<std::string, int> _symbols;
   int _variable_it;
   std::queue<int> _freed_variable_location;
+  std::vector<std::string> _preserved;
 };
+
 #endif // CONTEXT_HPP
